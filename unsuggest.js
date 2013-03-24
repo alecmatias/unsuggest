c59bb8cbff0c8ff18775f244990a868e36d998c0
Alec Matias <alec.matias@gmail.com>
2013-03-24 00:39:04 -0700

    Finalized version 0.1 of the extension, adding onload function to hide initially loaded Suggested posts.

--- unsuggest.js
+++ unsuggest.js
@@ -1,32 +1,28 @@
-// $('#contentArea').on('DOMSubtreeModified', function() {
-//   console.log("Dom ready")
-$('.uiUnifiedStory:contains("Laura")').css({backgroundColor: '#f00'});
-// });
+window.onload = function() {
+  var fauxEach = Array.prototype.forEach;
+  var initialPosts = document.querySelectorAll('.uiUnifiedStory');
 
-// Use document to listen to all events on the page (you might want to be more specific)
-// var $observerSummaryRoot = $("#contentArea");
-
-// function callback(summaries){
-//     console.log(summaries);
-// }
-
-// $observerSummaryRoot.mutationSummary("connect", callback, [{ all: true }]);
+  fauxEach.call(initialPosts, function(post) {
+    if (post.querySelector('a[href="\/about\/ads"]')) {
+      post.style.display = 'none';
+    }
+  });
+}
 
 var observer = new MutationSummary({
-  callback: handleChanges, // required
-  //rootNode: #contentArea, // optional, defaults to window.document
+  callback: hideSuggestedPosts, // required
+  rootNode: document.querySelector('#contentArea'), // optional, defaults to window.document
   queries: [
     { element: '.uiUnifiedStory' }
   ]
 });
 
-function handleChanges(summaries) {
-  var suggestedPosts = summaries[0];
-  suggestedPosts.added.forEach(function(newEl) {
-    console.log("HELLO" + newEl);
-    // if (newEl.has(':contains("James")')) {
-    //   newEl.hide();
-    // }
-    $(newEl).has('a[href="\/about\/ads"]').css({backgroundColor: '#f00'});
+function hideSuggestedPosts(summaries) {
+  var newPosts = summaries[0];
+  newPosts.added.forEach(function(post) {
+    if (post.querySelector('a[href="\/about\/ads"]')) {
+      post.style.display = 'none';
+      console.log('Hid a suggested post!')
+    }
   });
 }
